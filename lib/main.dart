9fead75b8f68f3fd34913dc0a1efc00ce3b090dc
javeedishaq <ishaqjaveed1@gmail.com>
2022-02-15 20:45:07 +0500

    row demo

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutterlayoutdemo/row_demo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        // Define the default brightness and colors.
+        brightness: Brightness.light,
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 20.0),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: RowDemo(),
+    );
+  }
+}
---
+++ lib/row_demo.dart
@@ -0,0 +1,87 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+
+class RowDemo extends StatefulWidget {
+  const RowDemo({Key? key}) : super(key: key);
+
+  @override
+  State<RowDemo> createState() => _RowDemoState();
+}
+
+class _RowDemoState extends State<RowDemo> {
+  MainAxisAlignment mainAxis = MainAxisAlignment.start;
+
+  List<DropdownMenuItem<String>> get dropdownItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      DropdownMenuItem(child: Text("Start"), value: "start"),
+      DropdownMenuItem(child: Text("Center"), value: "center"),
+      DropdownMenuItem(child: Text("End"), value: "end"),
+      DropdownMenuItem(child: Text("SpaceBetween"), value: "spacebetween"),
+      DropdownMenuItem(child: Text("SpaceAround"), value: "spaceAround"),
+      DropdownMenuItem(child: Text("SpaceEven"), value: "spaceEven"),
+    ];
+    return menuItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Row Demo'),
+        actions: [
+          DropdownButton(
+              hint: Text(
+                "Main Axis",
+                style: TextStyle(color: Colors.white),
+              ),
+              items: dropdownItems,
+              onChanged: (value) {
+                if (value == 'start') {
+                  setState(() {
+                    mainAxis = MainAxisAlignment.start;
+                  });
+                } else if (value == 'center') {
+                  setState(() {
+                    mainAxis = MainAxisAlignment.center;
+                  });
+                }
+              }),
+          // IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ],
+      ),
+      body: Container(
+        color: Colors.yellowAccent,
+        child: Row(
+          mainAxisAlignment: mainAxis,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.green,
+              width: 60,
+              height: 60,
+              child: Icon(Icons.star),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 60,
+              height: 60,
+              child: Icon(Icons.star),
+            ),
+            Container(
+              color: Colors.white,
+              width: 60,
+              height: 60,
+              child: Icon(Icons.star),
+            ),
+            Container(
+              color: Colors.pink,
+              width: 60,
+              height: 60,
+              child: Icon(Icons.star),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
